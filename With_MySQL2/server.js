fc6b28e51f4baced5273117131d97b8d05d15cd3
Stefan Momcilovic <52540681+stefanmomcilovic@users.noreply.github.com>
2022-01-19 13:57:22 +0100

    Update server.js

--- With_MySQL2/server.js
+++ With_MySQL2/server.js
@@ -142,7 +142,7 @@ app.prepare().then(async () => {
     console.log("---------- /USER ------------");
     // End of Getting our current user //
     // This shop hasn't been seen yet, go through OAuth to create a session
-      if (user == null || user.length == 0 || (user.shop == undefined || null)) {
+      if ((user == null || undefined) || user.length == 0 || (user.shop == undefined || null)) {
         ctx.redirect(`/auth?shop=${shop}`);
       } else {
         await handleRequest(ctx);
